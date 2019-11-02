f6909b0470b218f57838215aaae5e59d9ae1a6cc
Leonardo Santos Severino <leonardossev@gmail.com>
2019-11-02 06:13:19 -0300

    Implements the store method

--- src/controllers/TweetController.js
+++ src/controllers/TweetController.js
@@ -5,5 +5,11 @@ module.exports = {
     const tweets = await Tweet.find({}).sort('-createdAt');
 
     return res.json(tweets);
+  },
+
+  async store(req, res) {
+    const tweet = await Tweet.create(req.body);
+
+    return res.json(tweet);
   }
 }
