67f93b1fd657334f3cd99401d61ea00c9ac2b7a6
Leonardo Santos Severino <leonardossev@gmail.com>
2019-11-02 06:19:18 -0300

    Uses routes

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ mongoose.connect(stringConnection, {
   useUnifiedTopology: true
 })
 
+app.use(express.json());
+app.use(require('./routes'));
+
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
